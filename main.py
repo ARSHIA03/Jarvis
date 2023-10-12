748eece0131adf01e018395953b6584b27f16a05
Arshia Mathur <113014375+ARSHIA03@users.noreply.github.com>
2023-10-12 20:21:04 +0530

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import pyautogui
-
-while True :
-    pyautogui.keyDown('a')
-    pyautogui.keyDown('r')
-    pyautogui.keyDown('s')
-    pyautogui.keyDown('h')
-    pyautogui.keyDown('i')
-    pyautogui.keyDown('a')
